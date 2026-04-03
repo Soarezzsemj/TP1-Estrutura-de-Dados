9b5726a0b57b1fa10d2414dad21da66c924b9cd7
Soarezzsemj <popcornor6topster@outlook.com>
2026-04-03 19:38:22 -0300

    feat(lista): implementar estrutura do TAD e função de inicialização
    - Define a struct Processo com os campos do CSV conforme o roteiro
    
    - Define a struct Lista para gerenciamento de memória dinâmica
    
    - Implementa a função criar_lista no Prop.c usando malloc
    
    - Adiciona protótipos e proteções de header no Libs.h

--- Prop.c
+++ Prop.c
@@ -6,3 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Libs.h"
+
+Lista* criarLista(int cap) {
+    Lista* L = (Lista*)malloc(sizeof(Lista));
+    if (L != NULL) return NULL;
+
+    L->Capacidade = cap;
+    L->Tamanho = 0;
+
+    L->Dados = (Processo*) malloc(cap * sizeof(Processo));
+
+    if (L->Dados == NULL) {
+        free(L);
+        return NULL;
+    }
+
+    return L;
+}
